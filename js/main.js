db7694be1eb70ff1cce70a020635d2929a6eeafc
stonka123 <stonka123@gmail.com>
2022-08-24 18:38:37 +0200

    added axios , some js , sync api

--- js/main.js
+++ js/main.js
@@ -1,11 +1,50 @@
 const searchBtn = document.querySelector('.panel__wrapper-btn')
 const backBtn = document.querySelector('.app__footer-back')
 const panel = document.querySelector('.panel')
+const cityName = document.querySelector('.app__top-city')
 const appMain = document.querySelector('.app')
+const inputCity = document.querySelector('.panel__wrapper-input')
+const temperature = document.querySelector('.temperature')
+const weather = document.querySelector('.weather')
+const windValue = document.querySelector('.wind')
+
+const humidityValue = document.querySelector('.humidity')
+const warning = document.querySelector('.panel__wrapper-error')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = '&appid=a967b379e49952ca75115e9a5819ac5a'
+const API_UNITS = '&units=metric'
+const API_LANG = '&lang=pl'
+
+const getWeather = () => {
+	const city = inputCity.value
+	const URL = API_LINK + city + API_KEY + API_UNITS + API_LANG
+
+	axios
+		.get(URL)
+		.then(res => {
+			warning.textContent = ''
+			console.log(res)
+			const weath = res.data.weather[0].main
+			const wind = res.data.wind.speed
+
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			let unicodeDegCel = '℃'
+			let unicodeMetSec = '㎧'
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.round(temp) + ' ' + unicodeDegCel
+			weather.textContent = weath
+			windValue.textContent = wind.toFixed(1) + ' ' + unicodeMetSec
+			humidityValue.textContent = hum + ' ' + '%'
+		})
+		.catch(() => (warning.textContent = 'Wpisz nazwę miasta!'))
+}
 
 const showApp = () => {
 	panel.classList.add('translate-right')
 	appMain.classList.add('show')
+	getWeather()
 }
 
 const showPanel = () => {
